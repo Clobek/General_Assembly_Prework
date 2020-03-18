5c0f022681dfc01bde6b8674ff8daf81b08e0e7f
Bryce Belock <blizzardman1202@hotmail.com>
2020-03-18 16:43:52 -0500

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -23,22 +23,33 @@ const cards = [
 
 let cardsInPlay = [];
 
+function createBoard() {
+  for (let i = 0; i < cards.length; i++) {
+    let cardElement = document.createElement('img');
+    cardElement.setAttribute('src', 'images/back.png');
+    cardElement.setAttribute('data-id', i);
+    cardElement.addEventListener('click', flipCard);
+    document.getElementById('game-board').appendChild(cardElement);
+  }
+}
+
 function checkForMatch() {
     if (cardsInPlay[0] === cardsInPlay[1]) {
-        console.log("You found a match!");
+      setTimeout(function(){ alert("You found a match!"); }, 200);
       } else {
-        console.log("Sorry, try again.");
+        alert("Sorry, try again.");
       }
 }
 
-function flipCard(cardId) {
+function flipCard() {
+    let cardId = this.getAttribute('data-id');
     console.log("User flipped " + cards[cardId].rank);
     console.log(cards[cardId].cardImage);
     console.log(cards[cardId].suit);
     cardsInPlay.push(cards[cardId].rank);
+    this.setAttribute("src", cards[cardId].cardImage);
     if (cardsInPlay.length === 2){
         checkForMatch();
     }
 }
-flipCard(0);
-flipCard(2);
+createBoard();
